c44018cef68a54d8539679b1c71bd9d90b067de9
Christian Heimlich <chris@pcserenity.com>
2022-10-07 15:27:26 -0400

    Create beginnings of actual app main layout

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,19 +1,38 @@
 #include <QCoreApplication>
 
 // Core Includes
+#include "star/election.h"
 #include "star/reference.h"
+#include "star/calculator.h"
 
 int main(int argc, char *argv[])
 {
-    // Just dummy code for now
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
 
+    // Handle command line options...
+
+    // Just dummy paths for now
     QString ccPath = "C:/Users/Chris/Desktop/test_config.ini";
     QString bbPath = "C:/Users/Chris/Desktop/test_box.csv";
 
     QList<Star::Election> elections;
     Qx::GenericError electionLoadError = Star::electionsFromReferenceInput(elections, ccPath, bbPath);
-    qDebug() << electionLoadError.isValid();
+    if(electionLoadError.isValid())
+    {
+
+    }
+
+    // Create calculator
+    Star::Calculator calculator;
+
+    // Result container
+    QList<Star::ElectionResult> results;
+
+    // Calculate the results of each election
+    for(const Star::Election& election : elections)
+        results.append(calculator.calculateResult(election));
+
+    // Display results...
 
     return 0;
 }
