b15d77f0cccf3fd65a52161d7bfcb27533216738
Christian Heimlich <chris@pcserenity.com>
2022-10-22 15:53:22 -0400

    Change full reference election test to be data driven

--- tests/full_reference_election/tst_full_reference_election.cpp
+++ tests/full_reference_election/tst_full_reference_election.cpp
@@ -5,6 +5,9 @@
 #include <star/reference.h>
 #include <star/calculator.h>
 
+// Macros
+#define C_STR(q_str) q_str.toStdString().c_str()
+
 class tst_full_reference_election : public QObject
 {
     Q_OBJECT
@@ -18,7 +21,8 @@ private slots:
 //    void cleanupTestCase();
 
     // Test cases
-    void basic_election_sampler();
+    void various_ref_elections_data();
+    void various_ref_elections();
 
 };
 
@@ -26,21 +30,46 @@ tst_full_reference_election::tst_full_reference_election() {}
 //void tst_full_reference_election::initTestCase() {}
 //void tst_full_reference_election::cleanupTestCase() {}
 
-void tst_full_reference_election::basic_election_sampler()
-{    
-    // Load expected results
-    const QString erPath = QStringLiteral(":/data/basic_election_sampler.json");
+void tst_full_reference_election::various_ref_elections_data()
+{
+    // Setup test table
+    QTest::addColumn<QString>("bb_path");
+    QTest::addColumn<QString>("cc_path");
+    QTest::addColumn<QString>("er_path");
 
+    // Populate test table rows from file
+    QDir data(":/data");
+    QFileInfoList dataFiles = data.entryInfoList(QDir::NoFilter, QDir::Name);
+    QVERIFY(dataFiles.size() % 3 == 0);
+
+    for(qsizetype i = 0; i < dataFiles.size() - 2; i += 3)
+    {
+        const QFileInfo& bbFile = dataFiles[i];
+        const QFileInfo& ccFile = dataFiles[i + 1];
+        const QFileInfo& erFile = dataFiles[i + 2];
+
+        QVERIFY(bbFile.baseName() == ccFile.baseName() && bbFile.baseName() == erFile.baseName());
+        QVERIFY(bbFile.suffix() == "csv" && ccFile.suffix() == "ini" && erFile.suffix() == "json");
+
+        QTest::newRow(C_STR(bbFile.baseName())) << bbFile.filePath() << ccFile.filePath() << erFile.filePath();
+    }
+}
+
+void tst_full_reference_election::various_ref_elections()
+{
+    // Fetch data from test table
+    QFETCH(QString, bb_path);
+    QFETCH(QString, cc_path);
+    QFETCH(QString, er_path);
+
+    // Load expected results
     QList<Star::ExpectedElectionResult> expectedResults;
-    Star::ReferenceError expectedResultsLoadError = Star::expectedResultsFromReferenceInput(expectedResults, erPath);
+    Star::ReferenceError expectedResultsLoadError = Star::expectedResultsFromReferenceInput(expectedResults, er_path);
     QVERIFY2(!expectedResultsLoadError.isValid(), expectedResultsLoadError.errorDetails.toStdString().c_str());
 
     // Load reference elections
-    const QString ccPath = QStringLiteral(":/data/basic_election_sampler.ini");
-    const QString bbPath = QStringLiteral(":/data/basic_election_sampler.csv");
-
     QList<Star::Election> elections;
-    Star::ReferenceError electionLoadError = Star::electionsFromReferenceInput(elections, ccPath, bbPath);
+    Star::ReferenceError electionLoadError = Star::electionsFromReferenceInput(elections, cc_path, bb_path);
     QVERIFY2(!electionLoadError.isValid(), electionLoadError.errorDetails.toStdString().c_str());
 
     // Create calculator
