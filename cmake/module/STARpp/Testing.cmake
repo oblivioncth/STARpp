aa9839320769ee7554ec036e2d0e5e0a1ccd6900
Christian Heimlich <chris@pcserenity.com>
2022-12-28 14:58:47 -0500

    Fix usage of non-existant message(ERROR ...)

--- cmake/module/STARpp/Testing.cmake
+++ cmake/module/STARpp/Testing.cmake
@@ -7,7 +7,7 @@ macro(starpp_add_test)
     # Validate input
     if(__CUS_ADD_TEST_KEYWORDS_MISSING_VALUES)
         foreach(missing_val ${__CUS_ADD_TEST_KEYWORDS_MISSING_VALUES})
-            message(ERROR "A value for '${missing_val}' must be provided")
+            message(WARNING "A value for '${missing_val}' must be provided")
         endforeach()
         message(FATAL_ERROR "Not all required values were present!")
     endif()
